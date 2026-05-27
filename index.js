7568cb98554ad5bbeda4a7f2d40b5d70082c2929
Ashutosh Yadav <ashutoshyadav9580@gmail.com>
2026-05-27 23:25:27 +0530

    New Version

--- index.js
+++ index.js
@@ -1,6 +1,60 @@
-const menuToggle = document.getElementById("menuToggle");
-const navLinks = document.getElementById("navLinks");
+// script.js
 
-menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+// Admission Form
+
+document
+  .getElementById("admissionForm")
+  .addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    alert("Admission Form Submitted Successfully!");
+  });
+
+// Query Form
+
+document
+  .getElementById("queryForm")
+  .addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    alert("Your Query Has Been Sent!");
+  });
+
+// Scroll Animation
+
+const cards = document.querySelectorAll(
+  ".info-card, .gallery-item, .form-container"
+);
+
+window.addEventListener("scroll", () => {
+
+  cards.forEach(card => {
+
+    const cardTop = card.getBoundingClientRect().top;
+
+    if(cardTop < window.innerHeight - 100){
+
+      card.style.opacity = "1";
+      card.style.transform = "translateY(0)";
+    }
+
+  });
+
+});
+
+cards.forEach(card => {
+
+  card.style.opacity = "0";
+  card.style.transform = "translateY(50px)";
+  card.style.transition = "0.8s";
+
 });
