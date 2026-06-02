41154b0c8075ced3c00845e04f9b6d92401bab96
Ashutosh Yadav <ashutoshyadav9580@gmail.com>
2026-06-02 23:53:04 +0530

    SignUP

--- index.js
+++ index.js
@@ -268,3 +268,25 @@ document
     }
 
   });
+
+  function showLogin(){
+
+    document
+    .getElementById("loginForm")
+    .style.display="block";
+
+    document
+    .getElementById("registerForm")
+    .style.display="none";
+}
+
+function showRegister(){
+
+    document
+    .getElementById("loginForm")
+    .style.display="none";
+
+    document
+    .getElementById("registerForm")
+    .style.display="block";
+}
